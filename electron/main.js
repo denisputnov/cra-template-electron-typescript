88c130b665892f9958122c0659430b9ee93e3236
Денис Путнов <grnbows.connect@mail.ru>
2021-12-29 15:19:42 +0300

    fix/width 1000

--- electron/main.js
+++ electron/main.js
@@ -4,7 +4,7 @@ const url = require ('url')
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     webPreferences: {
       nodeIntegration: true
